58b2780c1222a2130e39aef0071e9acde9a55188
evavro <me@madhax.io>
2016-09-15 23:47:52 -0700

    :microscope: tests for terms

--- test/warble/lexer_test.clj
+++ test/warble/lexer_test.clj
@@ -3,6 +3,17 @@
             [warble.lexer :refer :all]))
 
 (deftest grammar-test
-  (testing "variable can be assigned"
-    (let [want [:root [:statement [:assign [:identifier ":Foo "] [:list]]]]]
-      (is (= (tokenize ":Foo = []") want)))))
+  (testing "variables"
+    (testing "can be assigned"
+      (let [want [:root [:statement [:assign [:identifier ":Test "] [:list]]]]]
+        (is (= (tokenize ":Test = []") want)))))
+  (testing "terms"
+    (testing "addition"
+      (let [want [:root [:statement [:add [:number "1"] [:number "2"]]]]]
+        (is (= (tokenize "1 + 2") want))))
+    (testing "division"
+      (let [want [:root [:statement [:div [:number "1"] [:number "2"]]]]]
+        (is (= (tokenize "1 / 2") want))))
+    (testing "complex operations"
+      (let [want [:root [:statement [:add [:number "1"] [:div [:number "2"] [:number "3"]]]]]]
+        (is (= (tokenize "1 + 2/3") want))))))
