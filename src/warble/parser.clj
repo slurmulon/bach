a83250ebfc19319ea1645035299fb270f1bbb282
evavro <me@madhax.io>
2016-09-17 11:16:23 -0700

    :truck: renamed root element to track

--- src/warble/parser.clj
+++ src/warble/parser.clj
@@ -1,2 +1,8 @@
 (ns warble.parser
   (:require [warble.lexer :refer tokenize]))
+
+; responsible for consuming an AST and producing a linear stream of data
+
+(defn contig-stream [ast stream]
+  (loop [node ast]
+    (cond (or :root :statement) (recur ))))
--- test/warble/lexer_test.clj
+++ test/warble/lexer_test.clj
@@ -4,27 +4,27 @@
 
 (deftest variables
   (testing "can be assigned"
-    (let [want [:root [:statement [:assign [:identifier ":Test"] [:number "1"]]]]]
+    (let [want [:track [:statement [:assign [:identifier ":Test"] [:number "1"]]]]]
       (is (= (tokenize ":Test = 1") want)))))
 
 (deftest terms
   (testing "addition"
-    (let [want [:root [:statement [:add [:number "1"] [:number "2"]]]]]
+    (let [want [:track [:statement [:add [:number "1"] [:number "2"]]]]]
       (is (= (tokenize "1 + 2") want))))
   (testing "division"
-    (let [want [:root [:statement [:div [:number "1"] [:number "2"]]]]]
+    (let [want [:track [:statement [:div [:number "1"] [:number "2"]]]]]
       (is (= (tokenize "1 / 2") want))))
   (testing "complex operations"
-    (let [want [:root [:statement [:add [:number "1"] [:div [:number "2"] [:number "3"]]]]]]
+    (let [want [:track [:statement [:add [:number "1"] [:div [:number "2"] [:number "3"]]]]]]
       (is (= (tokenize "1 + 2/3") want)))))
 
 (deftest list
   (testing "emptiness"
-    (let [want [:root [:statement [:list]]]]
+    (let [want [:track [:statement [:list]]]]
       (is (= (tokenize "[]") want))))
   (testing "multiple elements"
-    (let [want [:root [:statement [:list [:number "1"] [:number "2"]]]]]
+    (let [want [:track [:statement [:list [:number "1"] [:number "2"]]]]]
       (is (= (tokenize "[1, 2]") want))))
   (testing "can contain pairs"
-    (let [want [:root [:statement [:list [:pair [:number "1"] [:identifier ":Foo"]] [:pair [:number "2"] [:identifier ":Bar"]]]]]]
+    (let [want [:track [:statement [:list [:pair [:number "1"] [:identifier ":Foo"]] [:pair [:number "2"] [:identifier ":Bar"]]]]]]
       (is (= (tokenize "[1 -> :Foo, 2 -> :Bar]") want)))))
