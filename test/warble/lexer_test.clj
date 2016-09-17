168bfe3e6f127583b2bf21de40c84a5e77e07fcd
evavro <me@madhax.io>
2016-09-17 11:05:23 -0700

    :microscope: basic tests for terms and list

--- test/warble/lexer_test.clj
+++ test/warble/lexer_test.clj
@@ -2,18 +2,29 @@
   (:require [clojure.test :refer :all]
             [warble.lexer :refer :all]))
 
-(deftest grammar-test
-  (testing "variables"
-    (testing "can be assigned"
-      (let [want [:root [:statement [:assign [:identifier ":Test "] [:list]]]]]
-        (is (= (tokenize ":Test = []") want)))))
-  (testing "terms"
-    (testing "addition"
-      (let [want [:root [:statement [:add [:number "1"] [:number "2"]]]]]
-        (is (= (tokenize "1 + 2") want))))
-    (testing "division"
-      (let [want [:root [:statement [:div [:number "1"] [:number "2"]]]]]
-        (is (= (tokenize "1 / 2") want))))
-    (testing "complex operations"
-      (let [want [:root [:statement [:add [:number "1"] [:div [:number "2"] [:number "3"]]]]]]
-        (is (= (tokenize "1 + 2/3") want))))))
+(deftest variables
+  (testing "can be assigned"
+    (let [want [:root [:statement [:assign [:identifier ":Test"] [:number "1"]]]]]
+      (is (= (tokenize ":Test = 1") want)))))
+
+(deftest terms
+  (testing "addition"
+    (let [want [:root [:statement [:add [:number "1"] [:number "2"]]]]]
+      (is (= (tokenize "1 + 2") want))))
+  (testing "division"
+    (let [want [:root [:statement [:div [:number "1"] [:number "2"]]]]]
+      (is (= (tokenize "1 / 2") want))))
+  (testing "complex operations"
+    (let [want [:root [:statement [:add [:number "1"] [:div [:number "2"] [:number "3"]]]]]]
+      (is (= (tokenize "1 + 2/3") want)))))
+
+(deftest list
+  (testing "emptiness"
+    (let [want [:root [:statement [:list]]]]
+      (is (= (tokenize "[]") want))))
+  (testing "multiple elements"
+    (let [want [:root [:statement [:list [:number "1"] [:number "2"]]]]]
+      (is (= (tokenize "[1, 2]") want))))
+  (testing "can contain pairs"
+    (let [want [:root [:statement [:list [:pair [:number "1"] [:identifier ":Foo"]] [:pair [:number "2"] [:identifier ":Bar"]]]]]]
+      (is (= (tokenize "[1 -> :Foo, 2 -> :Bar]") want)))))
