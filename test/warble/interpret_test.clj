c1245ecb81a5d8ed71e8d2f94246c33b7facff38
evavro <me@madhax.io>
2016-10-20 22:03:27 -0700

    :microscope: valid identifier test (invalid identifier isn't working per spec)

--- test/warble/interpret_test.clj
+++ test/warble/interpret_test.clj
@@ -14,5 +14,13 @@
 (deftest validatation
   (testing "assignment"
     (let [want [:track [:statement [:assign [:identifier ":Test"] [:number "1"]]]]]
-        (is (= (validate want {}) true)))))
+      (is (= (validate want {}) true))))
+  ; (testing "re-assignment (not allowed)")
+  (testing "identifier (valid)"
+    (let [want [:track [:statement [:assign [:identifier ":A"] [:number "1"]]]
+                       [:statement [:assign [:identifier ":B"] [:identifier ":A"]]]]]
+      (is (= (validate want {}) true)))))
+  ; (testing "identifier (invalid, unknown variable)"
+  ;   (let [want [:track [:statement [:assign [:identifier ":A"] [:identifier ":Z"]]]]]
+  ;     (is (thrown-with-msg? Exception #"variable is never declared" (validate want {}))))))
 
