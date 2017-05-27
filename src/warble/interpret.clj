6c63491f5b9af2f047a3a1e508fbf52dac9ee118
evavro <me@madhax.io>
2017-05-27 02:41:55 -0700

    :construction: re-doing validation in interpret

--- src/warble/interpret.clj
+++ src/warble/interpret.clj
@@ -8,6 +8,34 @@
 (def powers-of-two (iterate (partial * 2) 1))
 
 (defn validate
+  [ast context]
+  (println "AST" ast)
+  (let [vars (get context :vars {})]
+    (letfn [(track-variable [label, value] (assoc context :vars (conj vars [label value])))]
+      (doseq [node ast]
+        (println "~node" node (vector? node))
+        (if (vector? node)
+          (let [token (first node) next-node (-> node rest first)]
+            (println "--- token" token)
+            (println "--- node" node)
+            (println "--- next-node ->>>>> " next-node)
+            (case token
+              ; :statement ; FIXME: remove need for this, should just fall to case default
+              ;   (println "STATEMENT" next-node)
+              :assign
+                (println "ASSIGN" next-node)
+              ; (:statement ; FIXME: remove need for this, should just fall to case default
+              ;   (println "STATEMENT" next-node))
+              ; (:assign
+              ;   (println "ASSIGN" next-node))
+              (validate next-node context))))))
+        ; (cond
+        ;   (seq? node) ()
+
+  true))
+
+
+(defn validate-BAK
   ; determine if variable assignments make sense. support hoisting.
   ; determine if beats/pairs align with defined tempo (simple base/modulus comparison should do the trick)
   ; ensure that keywords are invoked with valid arguments
@@ -15,15 +43,28 @@
   ; FIXME: ensure validate isn't getting recursively called too often (add a `print` under the first `let`)
   ; FIXME: use htps://clojuredocs.org/clojure.walk/walk instead of doseq
   [ast context]
+  (println "\nRecursing validate" ast)
+  (println "\trest ast" (next ast))
   (let [vars (get context :vars {})]
     (letfn [(track-variable [label, value] (assoc context :vars (conj vars [label value])))]
       (doseq [node ast]
-        (let [next-node (-> ast next first) go-next? (vector? next-node)]
+        (println "ast" ast)
+        ; TODO might need to revisit the vars here (TEST BELOW EXPRESSION)
+        ; (let [next-node (-> ast rest first) go-next? (vector? next-node)]
+        (let [next-node (-> ast next first) go-next? (vector? next-node)] ; true is just a test
           (when go-next?
+            (println "node, next-node" node next-node)
             (case node
               :assign
-                (validate next-node (track-variable next-node (next next-node)))
-              :identifier
+                (do
+                  (let [assignment (rest ast) assign-label (next next-node) assign-value (last assignment)]
+                    (println "Tracking variable" next-node (next next-node))
+                    (println "--- rest!" assign-label assign-value)
+                    ; (println "--- rest" (rest ast) (-> ast rest first) (-> ast rest last))
+                    ; (validate next-node (track-variable next-node (next next-node)))))
+                    ;(validate next-node (track-variable assign-label assign-value)))) ; SHOULD WORK (sort of)
+                    (validate next-node context))) ; EXPERIMENT
+              :identifier ; FIXME: needs to check for [:identifier :value], something like that
                 (let [has-var (contains? vars next-node)]
                   (cond (has-var) (validate next-node context) ; known variable, keep going
                         (not has-var) (validate next-node (track-variable next-node :empty)) ; register unknown variable
--- src/warble/translate.clj
+++ src/warble/translate.clj
@@ -3,6 +3,11 @@
 
 ; responsible for consuming an AST of nodes and producing a linear stream of data
 
-(defn contig-stream [nodes stream]
+; TODO: should result with:
+; beat -> [notes]*
+; AKA sliced-stream..?
+(defn continous-stream [nodes stream]
   (loop [node nodes] ; TODO: since pattern is "loop through arg1 and arg2 (node1 and node2), should prob use a reducer
     (cond (or :root :statement) (recur [node] (conj stream node)))))
+
+; TODO: fastest-beat (lowest-common-beat)
