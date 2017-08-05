e9c85bcec3fbe2606fc596c659c0fb50131c94ad
evavro <me@madhax.io>
2017-08-04 23:06:15 -0700

    :boom: removed redundant serialize module (all functionality is in translate)

--- src/warble/serialize.clj
+++
@@ -1,8 +0,0 @@
-(ns warble.serialize
-  (:require [clojure.data.json :as json]
-            [warble.interpret :as interp]))
-
-(defn to-json
-  [track]
-  (let [compiled-track (interp/compile-track track)]
-    (json/write-str track)))
