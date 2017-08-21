170896752a148529dc1351fb205690120ef80769
evavro <me@madhax.io>
2017-08-20 21:08:04 -0700

    :sparkles: removed old impl

--- src/warble/lexer.clj
+++ src/warble/lexer.clj
@@ -2,8 +2,5 @@
   (:require [instaparse.core :as insta]
             [clojure.java.io :as io]))
 
-; (def parse
-;   (insta/parser (slurp (str (System/getProperty "user.dir") "/grammar.bnf"))))
-
 (def parse
   (insta/parser (->> "grammar.bnf" io/resource slurp)))
