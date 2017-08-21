8aba676fb4d3f83343f78e354f026ccc12fe0678
evavro <me@madhax.io>
2017-08-20 21:07:38 -0700

    :truck: :cd: moved grammar file into ./resources

--- src/warble/lexer.clj
+++ src/warble/lexer.clj
@@ -1,5 +1,9 @@
 (ns warble.lexer
-  (:require [instaparse.core :as insta]))
+  (:require [instaparse.core :as insta]
+            [clojure.java.io :as io]))
+
+; (def parse
+;   (insta/parser (slurp (str (System/getProperty "user.dir") "/grammar.bnf"))))
 
 (def parse
-  (insta/parser (slurp (str (System/getProperty "user.dir") "/grammar.bnf"))))
+  (insta/parser (->> "grammar.bnf" io/resource slurp)))
