14bb4ef840d06044a10f609c6d2537b198be1bb2
Erik Vavro <me@madhax.io>
2020-09-30 17:02:29 -0400

    [2.0.0-SNAP] :recycle: :scroll: :sparkles: Improved organization of methods, docstrings, and created useful def aliases in bach.track

--- src/bach/track.clj
+++ src/bach/track.clj
@@ -51,7 +51,7 @@
                        [value-type] value-token]
                    (case value-type
                      :identifier
-                     (when (not (contains? (variables) value))
+                     (when (-> (variables) (contains? value) not)
                        (throw (Exception. (str "variable is not declared before it's used: " value ", " (variables)))))
                      (create-variable label value))))
        :div (fn [top-token bottom-token]
@@ -155,12 +155,16 @@
      track)
     @header))
 
+(defn get-tempo
+  "Determines the global tempo of the track. Localized tempos are NOT supported yet."
+  [track]
+  (find-header track "Tempo" default-tempo))
+
 (defn get-meter
   "Determines the global meter, or time signature, of the track. Localized meters are NOT supported yet."
   [track]
-  (let [reduced-track (reduce-values track)
-        header (find-header reduced-track "Meter" default-meter)]
-    header))
+  (let [reduced-track (reduce-values track)]
+    (find-header reduced-track "Meter" default-meter)))
 
 (defn get-meter-ratio
   "Determines the global meter ratio of the track.
@@ -168,11 +172,6 @@
   [track]
   (let [[beats-per-measure & [beat-unit]] (get-meter track)]
     (/ beats-per-measure beat-unit)))
-
-(defn get-tempo
-  "Determines the global tempo of the track. Localized tempos are NOT supported yet."
-  [track]
-  (find-header track "Tempo" default-tempo))
 
 (defn get-beat-unit
   "Determines the reference unit to use for beats, based on time signature."
@@ -248,6 +247,8 @@
    (get-total-beats track)
    (get-beat-unit track)))
 
+(def get-total-beat-units get-scaled-total-beats)
+
 (defn get-normalized-total-beats
   "Determines the total beats in a track normalized to the pulse beat of the track."
   [track]
@@ -257,11 +258,13 @@
      (max total-beats pulse-beat)
      (min total-beats pulse-beat))))
 
+(def get-total-pulse-beats get-normalized-total-beats)
+
 ; TODO: Consider removing. Useful for consistency and predictability but otherwise redundant.
 (defn get-total-measures
-  "Determines the total number of measures in the track.
+  "Determines the total number of measures defined in the track.
    Beats and measures are equivelant here since the beats are normalized to traditional semibreves/whole notes and crotchet/quarternotes.
-  In other words, a beat with a duration of 1 is equivalant to 4 quarter notes, or 1 measure in 4|4 time."
+  In other words, a beat with a duration of 1 is strictly equivalant to 4 quarter notes, or 1 measure in 4|4 time."
   [track]
   (get-total-beats track))
 
@@ -310,7 +313,7 @@
 (defn get-normalized-ms-per-beat
   "Determines the number of milliseconds each beat should be played for (normalized to the pulse beat).
    Primarily exists to make parsing simple and optimized in the high-level interpreter / player.
-   Referred to as 'normalized' because, as of now, all compiled beat durations (via `compile-track`) are normalized to the pulse beat.
+   Referred to as 'normalized' because, as of now, all beat durations (via `compose`) are normalized to the pulse beat.
    References:
      http://moz.ac.at/sem/lehre/lib/cdp/cdpr5/html/timechart.htm
      https://music.stackexchange.com/a/24141"
@@ -335,7 +338,7 @@
 (defn normalize-measures
   "Parses the track data exported via `!Play` into a normalized matrix where each row (measure) has the same number of elements (beats).
    Makes parsing the track much easier for the high-level interpreter / player as the matrix is trivial to iterate through.
-   Bach players can simply iterate one measure and beat at a time for `ms-per-pulse-beat` milliseconds on each interval.
+   Bach interpreters can simply iterate one measure and beat at a time for `ms-per-pulse-beat` milliseconds on each step.
    This strongly favors applications that must optimize and minimize their synchronization points."
   [track]
   (let [beat-cursor (atom 0)
@@ -359,21 +362,21 @@
                         (swap! beat-cursor + beats))
                       (update-measures [measure-index beat-index elements]
                         (swap! measures assoc-in [measure-index beat-index] elements))
-                      (beat-indices [beat]
+                      (beat-indices []
                         (let [global-beat-index @beat-cursor
                               local-beat-index (mod global-beat-index beats-per-measure)
                               measure-index (int (Math/floor (/ global-beat-index beats-per-measure)))]
                           {:measure measure-index :beat local-beat-index}))]
                 (insta/transform
                  {:pair (fn [duration elements]
-                          (let [beats (cast-duration duration)
-                                indices (beat-indices beats)
+                          (let [normalized-beat-duration (cast-duration duration)
+                                indices (beat-indices)
                                 measure-index (:measure indices)
                                 beat-index (:beat indices)
-                                normalized-items {:duration beats ; i.e. # of pulses to play items for
+                                normalized-items {:duration normalized-beat-duration
                                                   :items (cast-elements elements)}]
                             (update-measures measure-index beat-index normalized-items)
-                            (update-cursor beats)))}
+                            (update-cursor normalized-beat-duration)))}
                  play-track)))}
      reduced-track)
     @measures))
@@ -385,10 +388,10 @@
   (let [headers (get-headers track)
         meter (get-meter track)
         total-beats (get-total-beats track)
-        total-beat-units (get-scaled-total-beats track)
-        total-pulse-beats (get-normalized-total-beats track)
-        ms-per-pulse-beat (get-ms-per-beat track :pulse)
+        total-beat-units (get-total-beat-units track)
+        total-pulse-beats (get-total-pulse-beats track)
         ms-per-beat-unit (get-ms-per-beat track :unit)
+        ms-per-pulse-beat (get-ms-per-beat track :pulse)
         beat-unit (get-beat-unit track)
         pulse-beat (get-pulse-beat track)]
     (assoc headers
@@ -396,12 +399,13 @@
            :total-beats total-beats
            :total-beat-units total-beat-units
            :total-pulse-beats total-pulse-beats
-           :ms-per-pulse-beat ms-per-pulse-beat
            :ms-per-beat-unit ms-per-beat-unit
+           :ms-per-pulse-beat ms-per-pulse-beat
            :beat-unit beat-unit
            :pulse-beat pulse-beat)))
 
 ; TODO: Allow track to be provisioned in flat/stream mode (i.e. no measures, just evenly sized beats)
+; TODO: Add `version` property
 (defn provision
   "Provisions a parsed track, generating and organizating all of the information necessary to easily
    interpret a track as a single stream of normalized data (no references, all values are resolved and optimized)."
@@ -409,7 +413,6 @@
   (when (validate track)
     (let [headers (provision-headers track)
           data (normalize-measures track)]
-          ; TODO: version
       (struct playable-track headers data))))
 
 (defn compose
