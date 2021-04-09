9c0576a8c86ebf199048da3a7f7d5af938d84af1
Erik Vavro <me@madhax.io>
2021-04-08 20:37:31 -0400

    [v3] :package: :warning: Added missing nano-id package (sorry :see_no_evil:).

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
                  [org.clojure/clojurescript "1.8.34"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/tools.cli "1.0.194"]
-                 [instaparse "1.4.10"]]
+                 [instaparse "1.4.10"]
+                 [nano-id "1.0.0"]]
   :main ^:skip-aot bach.cli
   :target-path "target/%s"
   :plugins [[lein-bin "0.3.5"]
