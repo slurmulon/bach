09880b9dad8829b98fdd347e72bf50340b2a41f0
Erik Vavro <me@madhax.io>
2021-06-05 21:30:06 -0400

    [v3] :package: :wrench: Updated lein project version to 3.0.0-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject bach "2.1.0-SNAPSHOT"
+(defproject bach "3.0.0-SNAPSHOT"
   :description "Semantic music notation"
   :url "https://github.com/slurmulon/bach"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [;[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.8.34"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/tools.cli "1.0.194"]
